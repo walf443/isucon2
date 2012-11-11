527305d6acc569388dc3f4786773d8598cb00afb
Keiji, Yoshimi <walf443@gmail.com>
2012-11-12 08:01:27 +0900

    必要なページ用のcontrollerを作成

---
+++ webapp/scala/app/controllers/Admin.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Admin extends Controller {
+  def index() = TODO
+  def init_data() = TODO
+  def download_order_csv() = TODO
+}
+
---
+++ webapp/scala/app/controllers/Artist.scala
@@ -0,0 +1,8 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Artist extends Controller {
+  def show(artist_id: Int) = TODO
+}
