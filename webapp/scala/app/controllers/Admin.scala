5b477ea98bbf1a1353ac246a8b495a423be6415f
Keiji, Yoshimi <walf443@gmail.com>
2012-11-12 08:39:59 +0900

    redirectするように

--- webapp/scala/app/controllers/Admin.scala
+++ webapp/scala/app/controllers/Admin.scala
@@ -2,12 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import anorm.SQL
 
 object Admin extends Controller {
   def index() = Action {
     Ok(views.html.admin())
   }
-  def init_data() = TODO
+  def init_data() = Action {
+
+    Redirect(routes.Admin.index)
+  }
   def download_order_csv() = TODO
 }
 
