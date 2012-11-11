cea6d4dfc46b3b8589907f9f1f829ca2c24a37ea
Keiji, Yoshimi <walf443@gmail.com>
2012-11-12 08:47:44 +0900

    DBの初期化を実行できるように

--- webapp/scala/app/controllers/Admin.scala
+++ webapp/scala/app/controllers/Admin.scala
@@ -1,7 +1,10 @@
 package controllers
 
 import play.api._
+import scala.io.Source
 import play.api.mvc._
+import play.api.Play.current
+import play.api.db._
 import anorm.SQL
 
 object Admin extends Controller {
@@ -9,6 +12,14 @@ object Admin extends Controller {
     Ok(views.html.admin())
   }
   def init_data() = Action {
+    val source = Source.fromFile("../config/database/initial_data.sql")
+    DB.withConnection { implicit c =>
+      source.getLines.foreach( line =>
+        if ( line != "" ) {
+          SQL(line).execute()
+        }
+      )
+    }
 
     Redirect(routes.Admin.index)
   }
