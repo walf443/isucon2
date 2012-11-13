89980f0fa8c6af12adfa9d100df9b8684bd268e8
Keiji, Yoshimi <walf443@gmail.com>
2012-11-13 09:55:23 +0900

    とりあえずMockでtopをつくる

--- webapp/scala/app/controllers/Application.scala
+++ webapp/scala/app/controllers/Application.scala
@@ -2,9 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 
 object Application extends Controller {
   
-  def index = TODO
+  def index = Action {
+    val artists = Array(models.Artist(1, "NHN48"), models.Artist(2, "はだいろクローバーZ"))
+    Ok(views.html.index(artists))
+  }
   
 }
---
+++ webapp/scala/app/models/Artist.scala
@@ -0,0 +1,3 @@
+package models;
+
+case class Artist(id: Int, name: String)
